ee112270604e7e8c9fedbf5bcec4b9034a2c4c50
房镇 <2640347015@qq.com>
2020-09-29 16:32:07 +0800

    2020年9月29日16:32:01

---
+++ src/components/Administor/LiaoTianJiLu/LiaoTianJiLu.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="liaotianjilu-page">
+    <h3 style="position:absolute;top:55px;left:70px">聊天目录</h3>
+    <LiaoTianJiLuItem1 class="liaotianjilu-box1"></LiaoTianJiLuItem1>
+    <LiaoTianJiLuItem2 class="liaotianjilu-box2"> </LiaoTianJiLuItem2>
+    </div>
+</template>
+
+<script>
+import LiaoTianJiLuItem1 from './LiaoTianJiLuItem1'
+import LiaoTianJiLuItem2 from './LiaoTianJiLuItem2'
+export default {
+    components:{
+        'LiaoTianJiLuItem1':LiaoTianJiLuItem1,
+        'LiaoTianJiLuItem2':LiaoTianJiLuItem2
+    },
+}
+</script>
+
+<style>
+.liaotianjilu-page {
+  width: 1410px;
+  height: 1160px;
+  background-color: blue;
+
+}
+.liaotianjilu-box1{
+    width: 100px;
+    height: 30px;
+    position: absolute;
+    top: 70px;
+    right: 110px;
+    border-radius: 2px;
+    background-color: chartreuse;
+    /* display: inline-block; */
+}
+.liaotianjilu-box2{
+    width: 1320px;
+    height: 1060px;
+    position: absolute;
+    top: 105px;
+    left: 70px;
+    background-color: hotpink;
+}
+.liaotianjilu-h2{
+    display: inline;
+}
+</style>
