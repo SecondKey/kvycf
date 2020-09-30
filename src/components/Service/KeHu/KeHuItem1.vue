1f12f83da7771c88be3bef972bcf6b2754064fce
zyf <1242703910@qq.com>
2020-09-30 19:17:53 +0800

    test

--- src/components/Service/KeHu/KeHuItem1.vue
+++ src/components/Service/KeHu/KeHuItem1.vue
@@ -6,60 +6,68 @@
       <!-- 客户资料代码 -->
       <!-- 客户静态数据 -->
       <el-container >
-          <el-aside width="400px">
+       
+          <el-aside width="400px" :data="this.$store.state.SData_Company[this.$store.state.Data_Login_Conpany].client[1]">
                 <i class=" el-icon-s-custom" style="font-size: 300px; color: black;margin: 30px"></i>
-                <table style="color: #000;margin: 20px;" :data="data">
+               
+                <table style="color: #000;margin: 20px;">
                   <th>
-                    <tr class="KeHu_tr">姓名：<spon >{{name}}</spon></tr>
-                    <tr class="KeHu_tr">地址：<spon>{{position}}</spon></tr>
-                    <tr class="KeHu_tr">客户评级：<spon>{{}}</spon></tr>
+                    <tr class="KeHu_tr">姓名：{{name}}</tr>
+                    <tr class="KeHu_tr">地址：{{position}}</tr>
+                    <tr class="KeHu_tr">客户评级：{{rate}}</tr>
                   </th>
                 </table>
           </el-aside>
       <!-- 客户动态数据 -->
       <el-container>
-
           <el-main>
-            <el-table :data="data">
-              <el-table-column prop="visitTime" label="  ——历史访问时间——" width="180">
+            <el-table :data="this.$store.state.SData_Company[this.$store.state.Data_Login_Conpany].client[1]">
+              <el-table-column prop="visitTime" label="历史访问时间" width="180">
               </el-table-column>
-              <el-table-column  label="——邮箱——" width="180">{{tel}}
+              <el-table-column prop="email" label="邮箱" width="180">
               </el-table-column>
-              <el-table-column prop="tel" label="——手机——" width="180">
+              <el-table-column prop="tel" label="手机" width="180">
               </el-table-column>
             </el-table>
           </el-main>
           <el-footer height="300px">
           </el-footer>
       </el-container>
-        
 
+     
     </el-container>
 
 </template>
 
 <script>
+
+
+
+import KeHumsg from "./KeHumsg.js"
 export default {
+
    data() {
-     
       return {
-            id:this.$store.state.SData_Company[0].client[1].id, //k客户编号
-            name:this.$store.state.SData_Company[0].client[1].name, //客户名称
-            tel:this.$store.state.SData_Company[0].client[1].tel, //手机
-            email: '', //邮箱
-            position: this.$store.state.SData_Company[0].client[1].position, //地址
-            rate: '', //信誉评级
-            visitTime: '', //访问时间
-            order: [0, 1, 2, 3] //订单
+            // id: ,
+            // name: , //客户名称
+            // tel: , //手机
+            // email: , //邮箱
+            // position: , //地址
+            // rate: , //信誉评级
+            // visitTime: , //访问时间
+            // order: [0, 1, 2, 3] //订单
+
+            }
+        },
+         mounted:function() {
+          var _this=this
+          KeHumsg.on('val',function(m) {
+            _this.kk=m;
+          })
           
-      }
-      
-      },
-   
+        }
 
-      
-
-    }
+     }
 </script>
 
 
--- src/components/Service/KeHu/KeHuItem2.vue
+++ src/components/Service/KeHu/KeHuItem2.vue
@@ -2,27 +2,27 @@
   <el-aside width="300px" style="background-color: #ffffff">
 
     <!--  -->
-      <el-menu :default-openeds="['1', '3']">
+      <el-menu :default-openeds="['1', '3']" >
         <el-submenu index="1">
           <template slot="title"> <P class="Index_1st">普通用户</p></template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">用户1</el-menu-item>
-            <el-menu-item index="1-2">用户2</el-menu-item>
-            <el-menu-item index="1-3">用户3</el-menu-item>
+            <el-menu-item index="1-1" @click="menu1">{{name}}</el-menu-item>
+            <el-menu-item index="1-2" @click="menu2">{{name}}</el-menu-item>
+            <el-menu-item index="1-3">{{name}}</el-menu-item>
           </el-menu-item-group>
           </el-submenu>
         <el-submenu index="2">
           <template slot="title"><P class="Index_1st">高级用户</P></template>
           <el-menu-item-group>
-            <el-menu-item index="2-1">用户4</el-menu-item>
-            <el-menu-item index="2-2">用户5</el-menu-item>
+            <el-menu-item index="2-1">{{name}}</el-menu-item>
+            <el-menu-item index="2-2">{{name}}</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"><P class="Index_1st">至尊用户</P></template>
           <el-menu-item-group>
-            <el-menu-item index="3-1">用户6</el-menu-item>
-            <el-menu-item index="3-2">用户7</el-menu-item>
+            <el-menu-item index="3-1">{{name}}</el-menu-item>
+            <el-menu-item index="3-2">{{name}}</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
@@ -30,8 +30,40 @@
 </template>
 
 <script>
+import KeHumsg from './KeHumsg.js'
 export default {
+   data() {
+      return {
+            
+            id:this.$store.state.SData_Company[0].client[1].id, //k客户编号
+            name:this.$store.state.SData_Company[0].client[1].name,//客户名称
+            tel:this.$store.state.SData_Company[0].client[1].tel, //手机
+            email:this.$store.state.SData_Company[0].client[1].email, //邮箱
+            position:this.$store.state.SData_Company[0].client[1].position, //地址
+            rate: this.$store.state.SData_Company[0].client[1].rate, //信誉评级
+            visitTime: this.$store.state.SData_Company[0].client[1].visitTime, //访问时间
+            order: [0, 1, 2, 3] //订单
+            
+            }
+        },
+        mounted:function() {
+          var _this=this
+          KeHumsg.on('val',function(m) {
+            _this.name=m;
+          })
+          
+        },
 
+    methods: {
+      menu1:function(){
+        KeHumsg.$emit("val","this.$store.state.SData_Company[0].client[1]");
+      },
+      menu2:function(){
+        KeHumsg.$emit("val","this.$store.state.SData_Company[0].client[2]");
+      }
+   
+  },
+ 
 }
 </script>
 
