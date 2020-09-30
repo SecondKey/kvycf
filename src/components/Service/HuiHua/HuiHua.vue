1075db226d1ad27410e66dd45133a33c633ffa50
房镇 <2640347015@qq.com>
2020-09-30 19:33:12 +0800

    2020年9月30日19:32:26 改了router大小写问题 减少了一个会话页面组件

--- src/components/Service/HuiHua/HuiHua.vue
+++ src/components/Service/HuiHua/HuiHua.vue
@@ -3,7 +3,6 @@
         <HuiHuaItem1 class="huihua-box1">会话人表格</HuiHuaItem1>
         <HuiHuaItem2 class="huihua-box2">会话内容</HuiHuaItem2>
         <HuiHuaItem3 class="huihua-box3">会话人信息</HuiHuaItem3>
-        <HuiHuaItem4 class="huihua-box4">常用语</HuiHuaItem4>
     </div>
 </template>
 
@@ -11,13 +10,11 @@
 import HuiHuaItem1 from './HuiHuaItem1'
 import HuiHuaItem2 from './HuiHuaItem2'
 import HuiHuaItem3 from './HuiHuaItem3'
-import HuiHuaItem4 from './HuiHuaItem4'
 export default {
     components:{
         'HuiHuaItem1':HuiHuaItem1,
         'HuiHuaItem2':HuiHuaItem2,
-        'HuiHuaItem3':HuiHuaItem3,
-        'HuiHuaItem4':HuiHuaItem4
+        'HuiHuaItem3':HuiHuaItem3
     },
 }
 </script>
@@ -30,7 +27,7 @@ export default {
 }
 .huihua-box1{
     width: 240px;
-    height: 1100px;
+    height: 850px;
     background-color: hotpink;
     position: absolute;
     top: 50px;
@@ -46,18 +43,10 @@ export default {
 }
 .huihua-box3{
     width: 240px;
-    height: 480px;
+    height: 850px;
     background-color: cadetblue;
     position: absolute;
     top: 50px;
-    left: 1150px;
-}
-.huihua-box4{
-    width: 240px;
-    height: 600px;
-    background-color: blue;
-    position: absolute;
-    top: 550px;
     left: 1150px;
 }
 </style>
