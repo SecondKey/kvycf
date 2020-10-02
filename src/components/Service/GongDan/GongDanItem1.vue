c18adc45c102830969647f8ad474f427608d466d
kevinzhengzz <1725433499@qq.com>
2020-10-03 01:13:55 +0800

    2020-10-3 01:13:54

--- src/components/Service/GongDan/GongDanItem1.vue
+++ src/components/Service/GongDan/GongDanItem1.vue
@@ -3,7 +3,7 @@
 
     <!--  -->
       <el-menu :default-openeds="['1']">
-            <el-menu-item index="1-1" @click="handleClick(0)">全部工单</el-menu-item>
+            <el-menu-item index="1-1" @click="handleClick_all(0)">全部工单</el-menu-item>
             <el-menu-item index="1-2" @click="handleClick(0)">待处理的</el-menu-item>
             <el-menu-item index="1-3" @click="handleClick(1)">处理中工单</el-menu-item>
             <el-menu-item index="1-4" @click="handleClick(2)">已解决工单</el-menu-item>
@@ -20,13 +20,31 @@ export default {
   },
 
 methods:{
-    handleClick:function(state){
+    handleClick_all:function(state){
       // 从数组中找出和当前选择的用户的Order_List相同的元素
+    this.$store.state.currentOrder_List.length=0
     let Order_List = null;
     for(let i in this.Order_Lists){
       if(this.Order_Lists[i].state >= state){
         Order_List = this.Order_Lists[i];
-        this.$store.commit('currentOrder_List',Order_List)//将找出来的客户提交到state中
+        // this.$store.commit('currentOrder_List',Order_List)//将找出来的客户提交到state中
+        this.$store.state.currentOrder_List.push(Order_List)
+    console.log(this.$store.state.currentOrder_List);
+        }
+        if(i>7)
+        break;
+    }
+    
+  },
+  handleClick:function(state){
+    this.$store.state.currentOrder_List.length=0
+      // 从数组中找出和当前选择的用户的Order_List相同的元素
+    let Order_List = null;
+    for(let i in this.Order_Lists){
+      if(this.Order_Lists[i].state === state){
+        Order_List = this.Order_Lists[i];
+        // this.$store.commit('currentOrder_List',Order_List)//将找出来的客户提交到state中
+        this.$store.state.currentOrder_List.push(Order_List)
     console.log(this.$store.state.currentOrder_List);
         }
         if(i>7)
