0f58afc087615873b857f6ff47a5d8c4264c56a8
GDC <1956802791@qq.com>
2020-10-02 20:22:33 +0800

    20点22分 1002

--- src/components/Service/ShouYe/ShouYeItem1.vue
+++ src/components/Service/ShouYe/ShouYeItem1.vue
@@ -1,26 +1,42 @@
 <template>
-  <el-carousel indicator-position="outside">
+  <el-carousel indicator-position="none">
     <el-carousel-item v-for="item in 4" :key="item">
-      <h3>{{ item }}</h3>
     </el-carousel-item>
   </el-carousel>
 </template>
 
-<style>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
+<style scoped>
+  .el-carousel__item:nth-child(4n) {
+    background-image: url(./../../../assets/1.jpg);
+      width: 100%;
+      height: 443px;
+      background-repeat: no-repeat;
+      background-size: cover;
   }
   
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-    height: 443px;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
+  .el-carousel__item:nth-child(4n+1) {
     background-color: #d3dce6;
+    background-image: url(./../../../assets/2.jpg);
+        width: 100%;
+        height: 443px;
+        background-repeat: no-repeat;
+        background-size: cover;
   }
+  .el-carousel__item:nth-child(4n+2) {
+    background-color: #d3dce6;
+    background-image: url(./../../../assets/3.jpg);
+      width: 100%;
+      height: 443px;
+      background-repeat: no-repeat;
+      background-size: cover;
+  }
+  .el-carousel__item:nth-child(4n+3) {
+    background-color: #d3dce6;
+    background-image: url(./../../../assets/4.jpg);
+      width: 100%;
+      height: 443px;
+      background-repeat: no-repeat;
+      background-size: cover;
+  }
+
 </style>
