122b5de876b90e6674a82df85aeeffe682980900
SecondKey <2278228786@qq.com>
2020-10-03 10:49:17 +0800

    2020年10月3日10:49:15

--- src/components/Administor/SheZhi/SheZhi1.vue
+++ src/components/Administor/SheZhi/SheZhi1.vue
@@ -25,56 +25,45 @@
           <el-input
             disabled
             v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].id
-            "
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="昵称" style="margin-top:40px">
-          <el-input
-            v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].id
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].admin[
+                $store.state.Data_Login_ID
+              ].id
             "
           ></el-input>
         </el-form-item>
         <el-form-item label="姓名" style="margin-top:40px">
           <el-input
             v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].tName
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].admin[
+                $store.state.Data_Login_ID
+              ].name
             "
           ></el-input>
         </el-form-item>
         <el-form-item label="账号" style="margin-top:40px">
           <el-input
             v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].account
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].admin[
+                $store.state.Data_Login_ID
+              ].account
             "
           ></el-input>
         </el-form-item>
         <el-form-item label="密码" style="margin-top:40px">
           <el-input
             v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].pwd
-            "
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="电话" style="margin-top:40px">
-          <el-input
-            v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].tel
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].admin[
+                $store.state.Data_Login_ID
+              ].pwd
             "
           ></el-input>
         </el-form-item>
         <el-form-item label="邮箱" style="margin-top:40px">
           <el-input
             v-model="
-              $store.state.SData_Company[$store.state.Data_Login_Conpany]
-                .service[$store.state.Data_Login_ID].email
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].admin[
+                $store.state.Data_Login_ID
+              ].email
             "
           ></el-input>
         </el-form-item>
@@ -91,102 +80,90 @@
 
     <div class="settingArea" id="setting2">
       <el-divider></el-divider>
-      <div class="settingHead">常用语设置</div>
+      <div class="settingHead">企业信息</div>
       <el-divider></el-divider>
-      <el-table
-        :data="
-          this.$store.state.SData_Company[this.$store.state.Data_Login_Conpany]
-            .KeFuChangYongYu
-        "
-        style="width: 100%"
-        :show-header="false"
+      <el-form
+        ref="form"
+        label-width="80px"
+        style="width:500px;text-align:left;text-align: left;"
       >
-        <el-table-column prop="label" width="500"> </el-table-column>
+        <el-form-item label="企业名称" style="margin-top:40px">
+          <el-input
+            v-model="
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].name
+            "
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="公司联系人" style="margin-top:40px">
+          <el-input
+            v-model="
+              $store.state.SData_Company[$store.state.Data_Login_Conpany]
+                .principalName
+            "
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="公司账户" style="margin-top:40px">
+          <el-input
+            v-model="
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].email
+            "
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="电话" style="margin-top:40px">
+          <el-input
+            v-model="
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].tel
+            "
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="公司地址" style="margin-top:40px">
+          <el-input placeholder="请输入"></el-input>
+        </el-form-item>
+
+        <el-form-item label="备注信息" style="margin-top:40px">
+          <el-input placeholder="请输入"></el-input>
+        </el-form-item>
+
+        <el-form-item label="当前服务" style="margin-top:40px">
+          <el-input
+            disabled
+            v-model="
+              $store.state.SData_Company[$store.state.Data_Login_Conpany].scheme
+            "
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item style="margin-top:40px">
+          <el-button
+            type="success"
+            style="width:500px;height:50px;float:right;"
+          >
+            保存
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="settingArea" id="setting3">
+      <el-divider></el-divider>
+      <div class="settingHead">通知公告</div>
+      <el-divider></el-divider>
+      <el-table :data="allNotice" style="width: 100%" :show-header="false">
+        <el-table-column prop="notice" width="500"> </el-table-column>
+        <el-table-column prop="time" width="500"> </el-table-column>
       </el-table>
       <el-input
         style="width:400px"
-        v-model="nowChangYongYu"
-        placeholder="添加常用语"
+        v-model="noticeText"
+        placeholder="添加公告"
       ></el-input>
       <el-button
-        @click="AddChangYongYu"
+        @click="AddNotise"
         type="success"
         style="width:100px; margin-left:20px"
       >
         添加
       </el-button>
-    </div>
-
-    <div class="settingArea" style="width:500px" id="setting3">
-      <el-divider></el-divider>
-      <div class="settingHead">自动回复设置</div>
-      <el-divider></el-divider>
-      <div class="autoMargin">
-        欢迎消息
-        <el-switch
-          v-model="
-            $store.state.SData_Company[$store.state.Data_Login_Conpany]
-              .AutoReply.start
-          "
-        >
-        </el-switch>
-      </div>
-      <el-input
-        class="autoMargin"
-        type="textarea"
-        :rows="5"
-        v-model="
-          $store.state.SData_Company[$store.state.Data_Login_Conpany].AutoReply
-            .startMsg
-        "
-      ></el-input>
-      <div class="autoMargin">
-        自动回复
-        <el-switch
-          v-model="
-            $store.state.SData_Company[$store.state.Data_Login_Conpany]
-              .AutoReply.atuo
-          "
-        >
-        </el-switch>
-        <input
-          type="text"
-          :value="
-            $store.state.SData_Company[$store.state.Data_Login_Conpany]
-              .AutoReply.autoTime
-          "
-          style=" margin-left:10px;width:50px"
-        />
-        <div style="display:inline">ms</div>
-      </div>
-      <el-input
-        class="autoMargin"
-        type="textarea"
-        :rows="5"
-        v-model="
-          $store.state.SData_Company[$store.state.Data_Login_Conpany].AutoReply
-            .autoMsg
-        "
-      ></el-input>
-      <div class="autoMargin">
-        结束语
-        <el-switch
-          v-model="
-            $store.state.SData_Company[$store.state.Data_Login_Conpany]
-              .AutoReply.end
-          "
-        >
-        </el-switch>
-      </div>
-      <el-input
-        class="autoMargin"
-        type="textarea"
-        :rows="5"
-        v-model="
-          $store.state.SData_Company[$store.state.Data_Login_Conpany].AutoReply
-            .endText
-        "
-      ></el-input>
     </div>
 
     <div class="settingArea" id="setting4">
@@ -227,6 +204,130 @@
         <el-table-column prop="reason" label="拉黑原因"> </el-table-column>
       </el-table>
     </div>
+
+    <div class="settingArea" id="setting6">
+      <el-divider></el-divider>
+      <div class="settingHead">
+        对话规则
+      </div>
+      <el-divider></el-divider>
+
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        超时自动结束会话
+        <el-switch v-model="value"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">超过</div>
+          <input
+            type="text"
+            placeholder="10"
+            style=" margin-left:10px;width:50px"
+          />
+          <div style="display:inline">分钟未响应自动结束会话</div>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        访客离开自动结束会话
+        <el-switch v-model="value1"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">访客离线超过</div>
+          <input
+            type="text"
+            placeholder="10"
+            style=" margin-left:10px;width:50px"
+          />
+          <div style="display:inline">分钟自动结束对话</div>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        客服离线自动转接
+        <el-switch v-model="value2"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">访客发送消息后</div>
+          <input
+            type="text"
+            placeholder="10"
+            style=" margin-left:10px;width:50px"
+          />
+          <div style="display:inline">秒客服一直没有在线则自动转接</div>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        进入会话窗口自动开始对话
+        <el-switch v-model="value3"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">
+            开启后顾客进入对话窗时客服就会立即接入对话，客服可以主动与顾客沟通。关闭则必须顾客发送首条消息时客服才接入对话
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="settingArea" id="setting7">
+      <el-divider></el-divider>
+      <div class="settingHead">
+        分配规则
+      </div>
+      <el-divider></el-divider>
+
+      <el-radio-group
+        v-model="radio"
+        style="
+        background-color: aliceblue;"
+      >
+        <el-radio :label="0">
+          <div style="float:right">
+            <div>按排序轮流分配</div>
+            <div>
+              设定一个客服排序, 新的对话按此排序, 在可分配的客服间轮流分配,
+              从排序最靠前的开始
+            </div>
+          </div>
+        </el-radio>
+        <div></div>
+        <el-radio :label="1">
+          <div style="float:right">
+            <div>按排序轮流分配</div>
+            <div>
+              设定一个客服排序, 新的对话按此排序, 在可分配的客服间轮流分配,
+              从排序最靠前的开始
+            </div>
+          </div></el-radio
+        >
+        <el-radio :label="2">
+          <div style="float:right">
+            <div>按平均分配</div>
+            <div>
+              设定一个客服排序, 优先给排序靠前的客服分配到对话数量上限,
+              才会分配给排序下一位的客服
+            </div>
+          </div></el-radio
+        >
+      </el-radio-group>
+
+      <el-divider></el-divider>
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        回头客分配
+        <el-switch v-model="value4"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">
+            优先把回头客分配给上一次和他对话的客服
+          </div>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="autoMargin" style="margin-top:30px;margin-bottom:30px">
+        客户排队超时优先
+        <el-switch v-model="value5"> </el-switch>
+        <div style="margin-top:30px">
+          <div style="display:inline">
+            客户发起咨询排队时，如排队等待超过设置的时间，系统将优先接入
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -234,16 +335,55 @@
 export default {
   data() {
     return {
-      nowChangYongYu: '',
+      noticeText: '',
 
-      sortList: [1, 2, 3, 4]
+      sortList: [1, 2, 3, 4],
+      allNotice: [
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        },
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        },
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        },
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        },
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        },
+        {
+          notice: 'WCT-24新品上线',
+          time: '2020/10/3 上午10:00:14'
+        }
+      ],
+      value: true,
+      value1: true,
+      value2: true,
+      value3: true,
+
+      radio: 0,
+      value4: true,
+      value5: true
     }
   },
   methods: {
-    AddChangYongYu() {
-      this.$store.commit('SData_AddChangYongYu', this.nowChangYongYu)
-      this.nowChangYongYu = ''
-      this.sortList.sort()
+    AddNotise() {
+      let myDate = new Date()
+      let n = {
+        notice: this.noticeText,
+        time: myDate.toLocaleString()
+      }
+      this.allNotice.push(n)
+      this.allNotice.sort()
+      this.noticeText = ''
     },
     onScroll(event) {
       let i = 0
@@ -265,7 +405,7 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
 .settingArea {
   margin-left: 30px;
   padding-top: 30px;
@@ -282,7 +422,7 @@ export default {
 }
 .el-form-item__label {
   text-align: left !important;
-  font-size: 30px !important;
+  font-size: 13px !important;
 }
 .autoMargin {
   margin-top: 10px;
